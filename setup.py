ce5fea5f6c91452f59aa207b4296f4a260a35da5
Andreas Jung <info@zopyx.com>
2021-12-19 16:17:42 +0100

    iupdated

--- setup.py
+++ setup.py
@@ -51,11 +51,8 @@ setup(
         'z3c.jbot',
         'Products.GenericSetup>=1.8.2',
         'plone.api>=1.8.4',
-        'plone.restapi < 7.0.0a1',
+        'plone.restapi ',
         'plone.app.dexterity',
-        'plone.app.referenceablebehavior',
-        'plone.app.relationfield',
-        'plone.app.lockingbehavior',
         'plone.schema',
     ],
     extras_require={
