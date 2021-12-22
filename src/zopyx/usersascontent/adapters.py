6952eeef2003c126aba5a7f1559f132cfd481c98
Andreas Jung <info@zopyx.com>
2021-12-22 12:52:13 +0100

    minor refactoring

--- src/zopyx/usersascontent/adapters.py
+++ src/zopyx/usersascontent/adapters.py
@@ -2,6 +2,7 @@ from plone import api
 from Products.CMFPlone.interfaces import IRedirectAfterLogin
 from Products.CMFPlone.utils import safe_unicode
 from zope.interface import implementer
+from zopyx.usersascontent.interfaces import IUsersAsContentSettings
 
 
 @implementer(IRedirectAfterLogin)
@@ -11,26 +12,30 @@ class RedirectAfterLoginAdapter(object):
         self.context = context
         self.request = request
 
-    def __call__(self, came_from=None, is_initial_login=False):
+
+    def create_and_get_user_folder(self):
 
         portal = api.portal.get()
-        url = portal.absolute_url() + "/news"
+        user = api.user.get_current()
+        user_id = user.getId()
+        user_folder_id = api.portal.get_registry_record("user_folder_id", IUsersAsContentSettings)
 
         with api.env.adopt_roles(roles=["Manager"]):
 
-            if 'users' not in portal.objectIds():
-                uf = api.content.create(type="Folder", id="users", title="Users", container=portal)
+            if user_folder_id not in portal.objectIds():
+                uf = api.content.create(type="Folder", id=user_folder_id, title="Users", container=portal)
                 api.content.transition(uf, "publish")
-            uf = portal["users"]
+            uf = portal[user_folder_id]
 
-            user = api.user.get_current()
-            user_id = user.getId()
 
             if user_id not in uf.objectIds():
                 api.portal.show_message("We created a new user object for you", self.request)
                 user_obj = api.content.create(type="PloneUser", id=user_id, title=user_id, container=uf)
 
-        user_obj = uf[user_id]
+        return portal[user_folder_id][user_id]
 
+
+    def __call__(self, came_from=None, is_initial_login=False):
+        user_obj = self.create_and_get_user_folder()
         return user_obj.absolute_url()
 
