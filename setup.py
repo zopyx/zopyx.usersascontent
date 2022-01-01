33735720dbfacc0681b667d247747739893ba85b
Andreas Jung <info@zopyx.com>
2022-01-01 10:15:54 +0000

    updated

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='zopyx.usersascontent',
-    version='1.0a1',
+    version='1.0a2',
     description="Plone users as content",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
